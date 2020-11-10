499555f8f1aea52ac4705ea0b964cd5c395c9fc0
PennyAna <law07006@byui.edu>
2020-11-09 18:35:21 -0700

    oops

--- week6/ToDos.js
+++ week6/ToDos.js
@@ -2,11 +2,7 @@ import utils from "./utilities.js";
 import ls from "./ls.js";
 
 //from instructor vides
-document.querySelector("addBtn").onclick = newTask;
-const todo = createTodo();
-const todoDiv = createTodoElement(todo);
-addToList(todoDiv);
-ls.saveTodo(todo);
+document.querySelector("addBtn").onclick = newTodo;
 
 function loadTodos() {
     const todoList = ls.getTodoList();
@@ -61,87 +57,6 @@ function deleteTodo(e) {
     loadTodos();
 }
 
-
-export default class Tasks {
-    constructor(elementId) {
-        this.parentElement = document.getElementById(elementId);
-    }
-    //getter functions
-    //grab whole TodoList
-    //grab single/next taskItem in TodoList
-    getSingle(){
-
-    }
-    //filter functions
-    //return new list of checkboxes == false
-    filterActive(){
-
-    }
-    //return new list of checkboxes == true
-    filterDone(){
-
-    }
-    removeSingle(){
-    //use taskNumbering() to reset task num
-    }  
-    addSingle(){
-    //use taskNumbering() to reset task num
-    var todoName = document.getElementById("").nodeValue;//key
-    var todoHTML = document.getElementById("").nodeValue;//task info
-    localStorage.writeToLS(todoName, todoHTML); //key, task info 
-    }
-     //returns number of tasks left/counts number of taskItems in TodoList
-    taskNumbering(){
-        return TodoList.length();
-    }
-};
-//outside of the class we put the event listeners tied to the buttons
-//these have access to the Task functions because we'll create a new Task object for them to work with. 
-const taskItem = {
-    id: timestamp, 
-        content:string, 
-        completed: bool
-};
-
-const TodoList = new Array[taskItem];
-
-const doneList = new Array[taskItem];
-
-const activeList = new Array[taskItem];
-  
- 
-    //render functions
-    //display all taskItems in TodoList with html et checkboxes
-    renderAll(){
-        //needs getAll
-    }
-    //display all taskItems in TodoList returned by filterActive with html et checkboxes
-    renderActive(){
-        //needs filterActive
-    }
-    //display all taskItems in TodoList returned by filterDone with html et checkboxes
-    renderDone(){
-        //needs filterDone
-    }
-    //display single/next taskItem in TodoList with html et checkboxes
-    renderSingle(){
-
-    }
-    //event listeners
-    //for checkboxes
-    //also needs to cross out task description
-    
-    //for 'x' (close task) button
-    //uses removeSingle() 
-
-    //for all button
-    //uses getAll()->renderAll()
-
-    //for active button
-    //uses getAll()->filterActive()->renderSingle() loop
-
-    //for completed button
-    //uses getAll()->filterDone()->renderSingle() loop
 
     
 
