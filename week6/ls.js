d5e16ef2c3c85268507769c9bdc6bbee2aafb2ba
PennyAna <law07006@byui.edu>
2020-11-09 18:48:08 -0700

    took out old code

--- week6/ls.js
+++ week6/ls.js
@@ -19,16 +19,3 @@ function getTodoList(){
 export default {
     saveTodo, getTodoList, deleteTodo
 }
-//read val from localstorage and parse as JSON @param{string} key 
-//the key under which the val is stored under in LS
-//@return {array} the val as an array of objects
-export function readFromLS(key) {
-
-}
-
-//write an array of objects to local storage under provided key @ param {string} key
-//the key under which the val is stored under in LS
-//@param{array} data the info stored as array of objects must be serialized
-export function writeToLS(key, data) {
-    
-}
--- week6/utilities.js
+++ week6/utilities.js
@@ -1,16 +1,4 @@
 export default {
-    
-}
-
-//do querySelector lookup @param {string} selector
-//selector passed to querySelector
-//@return {element} matching element or null if not found
-export function qs(selector) {
 
 }
-//add touchend event listener to an element for mobile with click event fallback for desktops
-//@param{string} elementSelector selector for element to attach listener to
-//@param{function} callback function to run
-export functionOnTouch(elementSelector, callback){
 
-}
