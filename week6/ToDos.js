8df1bf204278126d98306eb9fbe25d1efc726e37
PennyAna <law07006@byui.edu>
2020-11-09 19:35:30 -0700

    wdkjflds

--- week6/ToDos.js
+++ week6/ToDos.js
@@ -2,7 +2,7 @@ import utils from "./utilities.js";
 import ls from "./ls.js";
 
 //from instructor vides
-document.querySelector('#addBtn').onclick = newTodo;
+document.querySelector('#addBtn').onclick = newTodo();
 
 function loadTodos() {
     const todoList = ls.getTodoList();
@@ -18,7 +18,7 @@ function newTodo() {
 }
 function createTodo() {
     const input = document.querySelector("addBtn");
-    const newTodo = { id: Date.now(), content: input.value, completed: false};
+    const newTodo = { id: Date.now(), content: input.value, completed: false}
     input.value = '';
     return newTodo;
 }
