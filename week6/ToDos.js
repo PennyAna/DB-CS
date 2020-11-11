01e375a8a729bdd33819cb3002ba455a8f97fd56
PennyAna <law07006@byui.edu>
2020-11-10 20:17:57 -0700

    dfsd

--- week6/ToDos.js
+++ week6/ToDos.js
@@ -32,7 +32,8 @@ function createTodoElement(todo) {
     //complete btn
     const completeBtn = document.createElement('button');
     completeBtn.classList.add('complete-btn');
-    completeBtn.onclick = function (todo) {todo.completed = true; todo.content.strike();}
+    completeBtn.onclick = function (todo) {todo.completed = true;}
+    console.log(todo.completed);
     //todo content
     const todoContent = document.createElement('div');
     todoContent.innerText = todo.content;
