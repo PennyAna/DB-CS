587a2dd6ab6289527c0a5240a08188bcc1bdb05c
PennyAna <law07006@byui.edu>
2020-11-13 20:56:46 -0700

    lklk

--- week9/animation/main.js
+++ week9/animation/main.js
@@ -9,5 +9,4 @@ const squareElementTwo = document.getElementById('squareTwo');
 let angleTwo = 0;
 function rotate() {
     angleTwo = (angleTwo + 2) % 360;
-    squareElementTwo.style.transform = `rotate(${angleTwo}deg)`;
 window.requestAnimationFrame(rotate); }
