012956c1f0572446eecfbacdcd6e4edbd3cf042f
PennyAna <law07006@byui.edu>
2020-11-13 21:07:55 -0700

    final fix sq2

--- week9/animation/main.js
+++ week9/animation/main.js
@@ -4,12 +4,3 @@ setInterval( ()=>{
     angleOne = (angleOne + 2) % 360;
     squareElementOne.style.transform = `rotate(${angleOne}deg)`
 }, 1000/60);
-
-const squareElementTwo = document.getElementById('squareTwo');
-let angleTwo = 0;
-function rotate() {
-    angleTwo = (angleTwo + 2) % 360;
-    squareElementOne.style.transform = `rotate(${angleOne}deg)`
-    window.requestAnimationFrame(rotate);
-}
-const id = requestAnimationFrame(rotate);
