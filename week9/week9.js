f11c9f1750199447dcc14c9860ac9f14f591e291
PennyAna <law07006@byui.edu>
2020-11-13 23:21:13 -0700

    sds

--- week9/week9.js
+++ week9/week9.js
@@ -7,13 +7,8 @@ function dialogs() {
     const fearBool = String(window.confirm("Did you fear that alert?"));
     const fearLevel = String(window.prompt("Using a scale of 1-10, how much did you fear the alert?"));
     const fearDiv = document.createElement('div');
-    const fearResults = "";
-    if (fearBool) {
-         fearResults = `You feared me by a magnitude of ${fearLevel}`;
-    }
-    else {
-        fearResults = `You didn't fear me even a little???`;
-    }
-    fearDiv = innerHTML = fearResults;
-    document.body.appendChild(fearDiv);
+    const fearDivParent = document.getElementById("dialogBtn");
+    const fearResults = fearBool.concat(fearLevel);
+    fearDiv.innerHTML = fearResults;
+    fearDivParent.appendChild(fearDiv);
 }
