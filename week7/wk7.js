e6345b608668edd6de9be8b852459b74b9dd0bd0
PennyAna <law07006@byui.edu>
2020-11-22 21:52:01 -0700

    et again

--- week7/wk7.js
+++ week7/wk7.js
@@ -7,17 +7,17 @@ const apiURL = 'https://api.chucknorris.io/jokes/random';
 
 textButton.addEventListener('click', () => {
     fetch(textURL)
-    .then(response => {
+    .then( response => {
         outputDiv.innerHTML = 'Waiting for response...';
-        if(response.ok) {
-            return response;
-        }
+    if(response.ok) {
+        return response;
+    }
         throw Error(response.statusText);
     })
-    .then(response => response.text())
-    .then(text => outputDiv.innerText = text)
-    .catch(error => console.log('There was an error:', error))
-}, false);
+    .then( response => response.text() )
+    .then( text => outputDiv.innerText = text )
+    .catch( error => console.log('There was an error:', error))
+},false);
 
 apiButton.addEventListener('click', () => {
     fetch(apiURL)
